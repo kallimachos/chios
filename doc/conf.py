42a120ecbb550401a6d95132a6858f5cf27eb00b
Brian Moss <kallimachos@gmail.com>
2019-11-08 13:48:01 +1000

    Update copyright in docs

--- doc/conf.py
+++ doc/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'chios'
-copyright = '2017, Brian Moss'
+copyright = '2019, Brian Moss'
 author = 'Brian Moss'
 
 # The version info for the project you're documenting, acts as replacement for
